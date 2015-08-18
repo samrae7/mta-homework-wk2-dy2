63ea1a1c16686a05da43e8bc7c852175a0513fe1
Sam Rae <samuelrae@gmail.com>
2015-08-18 23:28:52 +0100

    got two lines working

--- js/mta.js
+++ js/mta.js
@@ -1,33 +1,61 @@
 window.onload = function() {
 
-// banana = document.getElementById('heading');
+  // banana = document.getElementById('heading');
 
-// function newFunction() {
+  // function newFunction() {
 
-// apple = document.getElementById('testing');
-// console.log(banana);
-// console.log(apple);
-// };
+  // apple = document.getElementById('testing');
+  // console.log(banana);
+  // console.log(apple);
+  // };
 
-// newFunction();
+  // newFunction();
 
-var nLine = ['times-square', '34thN', '28thN', '23rdN', 'union-square', '8thN'];
-
-
-document.getElementById('calcButton').addEventListener('click',function(){
-  var startStation = document.getElementById('startList').value;
-  var endStation = document.getElementById('endList').value;
-  distance(startStation, endStation);
-});
+  // var nLine = ['times-square', '34thN', '28thN', '23rdN', 'union-square', '8thN'];
 
 
-function distance(start,end) {
-  var iStart= nLine.indexOf(start);
-  var iEnd = nLine.indexOf(end);
-  var noStops = Math.abs(iEnd-iStart);
-  var message = noStops===1 ? '1 stop' : noStops+' stops'; 
-  console.log('Take the N line for '+message);
-  return('Take the N line for '+message);
-};
+  subway = {
+    nLine: ['times-square', '34thN', '28thN', '23rdN', 'union-square', '8thN'],
+
+    lLine: ['8thL','6thL','union-square','3rdL','1stL']
+  }
+
+  document.getElementById('calcButton').addEventListener('click',eventListener);
+
+
+  function eventListener() {
+    //declare noOfStop variable
+    var noOfStops
+    //get start
+    var startStation = document.getElementById('startList').value;
+    //get end station
+    var endStation = document.getElementById('endList').value;
+    //what line is startStation in? Store in variable
+    var startLine = subway.nLine.indexOf(startStation)===-1 ? subway.lLine : subway.nLine;
+    //what line is endStation in? Store in variable
+    var endLine = subway.nLine.indexOf(endStation)===-1 ? subway.lLine : subway.nLine;
+
+    //if they're the same call distanceOnLine(a.b.c)
+    if (startLine===endLine) {
+      noOfStops = distance(startStation, endStation, startLine);
+
+      } else {
+        //if the're different call distance OnLine on both of them using union-square as the other value. Then add the reusult together
+      noOfStops = distance(startStation, 'union-square', startLine) + distance(endStation, 'union-square', endLine);
+      
+    };
+
+    var message = noOfStops===1 ? '1 stop' : noOfStops+' stops';
+
+    console.log('You have ' + message+' to your destination');
+
+  };
+
+
+  function distance(start,end,line) {
+    var iStart= line.indexOf(start);
+    var iEnd = line.indexOf(end);
+    return Math.abs(iEnd-iStart);
+  };
 
 };
